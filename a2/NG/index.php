34f466666790f3e836cf931bd350ee3c8baca1cc
s3723996 <s3723996@student.rmit.edu.au>
2018-08-31 17:18:26 +1000

    File and Folder Shuffeling to create new site because concept has changed = GOOD DESIGN

--- a2/NG/index.php
+++ a2/NG/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Assignment 2</title>
-    
+
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
