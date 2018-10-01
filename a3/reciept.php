56af0839b4fd2611687f772535ae5e1f2ff1dd7d
s3723996 <s3723996@student.rmit.edu.au>
2018-10-01 15:43:51 +1000

    Reciept page created

---
+++ a3/reciept.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <title>STORE</title>
+</head>
+
+<body>
+    <header id="product_header">
+        <div class="logo">
+            <a href="index.php">
+            <?php echo '<img src="A2media/Logo.png" id="mobile"/>';?>
+
+            <?php echo '<img src="A2media/Logo.png" id="desktop"/>';?>
+            </a>
+        </div>
+    </header>
+
+    <div class="nav_bar">
+        <ul id="navul">
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="product.php">STORE</a></li>
+            <li><a href="lookbook.php">SS'19</a></li>
+            <li><a href="login.php">LOGIN</a></li>
+        </ul>
+    </div><br>
+    <h2>RECIEPT PAGE</h2>
+</body>
+<footer>
+    &copy; Copyright 2018 PIECES by YJ, Saadiya Fourie [s3723996]
+</footer>
+
+</html>
