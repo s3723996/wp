d884cdebc2f70680038dc418c1e649394e609731
s3723996 <s3723996@student.rmit.edu.au>
2018-10-01 15:37:13 +1000

    file renaming

--- a3/index.php
+++ a3/index.php
@@ -1,51 +1,52 @@
 <!DOCTYPE html>
-<html lang='en'>
-  <head>
-    <meta charset="utf-8">
-    <title>Assignment 3</title>
+<html>
 
-    <!-- Keep wireframe.css for debugging, add your css to style.css -->
-    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
-    <script src='../wireframe.js'></script>
-    <style>
-      <?php
-        // This PHP code inserts CSS to style the "current page" link in the nav area
-        $here = $_SERVER['SCRIPT_NAME']; 
-        $bits = explode('/',$here); 
-        $filename = $bits[count($bits)-1]; 
-        echo "nav a[href$='$filename'] {
-        box-shadow: 1px 1px 1px 2px navy;
-      }";
-      ?>
-      
-    </style>
-  </head>
+<head>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <title>PIECES by YJ</title>
+</head>
 
-  <body>
+<body id="index_body">
 
     <header>
-      <div>Put company logo and name here</div>
+        <div class="logo">
+            <a href="index.php">
+            <?php echo '<img src="A2media/Logo.png" id="mobile"/>';?>
+
+            <?php echo '<img src="A2media/Logo.png" id="desktop"/>';?>
+
+            </a>
+        </div>
+
     </header>
 
-    <nav>
-      <div>Put website navigation links here</div>
-    </nav>
 
-    <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
-    </main>
 
-    <footer>
-      <div>&copy;<script>
-        document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here.</div>
-      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-      <div>Maintain links to your <a href='products.txt'>products spreadsheet</a> and <a href='orders.txt'>orders spreadsheet</a> here. <button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-    </footer>
-   
-  </body>
+    <div class="nav_bar">
+        <ul id="navul">
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="product.php">STORE</a></li>
+            <li><a href="lookbook.php">SS'19</a></li>
+            <li><a href="login.php">LOGIN</a></li>
+        </ul>
+    </div><br>
+
+
+    <div id="blurb">
+        <h2>An explorative label of neutrality based in Melbourne, Australia.</h2>
+    </div>
+
+
+
+    <div class="box1">
+
+        <?php echo '<img src="A2media/bodyimg.png" id="mainimg"/>'; ?>
+        <?php echo '<img src="A2media/bodyimg.png" id="mainimg"/>'; ?>
+    </div>
+</body>
+
+<footer>
+    &copy; Copyright 2018 PIECES by YJ, Saadiya Fourie [s3723996]
+</footer>
+
 </html>
