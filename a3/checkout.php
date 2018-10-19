3101d6eb439082deb4085c19b1e24bcc111314ba
s3723996 <s3723996@student.rmit.edu.au>
2018-10-19 23:36:58 +1100

    Price array created and function to calculate cart total

--- a3/checkout.php
+++ a3/checkout.php
@@ -12,13 +12,45 @@ else if (isset($_POST["delete"])){
 }
 
 // print_r ($_POST);
-print_r ($_SESSION['cart']);
+//print_r ($_SESSION['cart'][0]["Title"]);
 
 ?>
 
 <a href="products.php">
 	<input type="button" value="BACK TO STORE">
 </a>
+
+
+<?php
+//print_r ($_SESSION['cart']);
+$cartLength = count($_SESSION['cart']);
+$priceArray =array();
+$i = 0;
+for ($i; $i < $cartLength; $i++){
+    $id = $_SESSION['cart'][$i]['ID'];
+    $title = $_SESSION['cart'][$i]['Title'];
+    $price = $_SESSION['cart'][$i]['Price'];
+
+    array_push ($priceArray, $price);
+
+
+echo <<<DISP
+    <p>$title $$price</p>
+DISP;
+}
+//print_r ($priceArray);
+//function cartTotal($priceArray, $cartTotal) {
+//    $cartTotal = array_sum($priceArray);
+//    return $cartTotal;
+
+$cartTotal = 0;
+$cartTotal = number_format(array_sum($priceArray), 2, ".", " ");
+echo <<<DISP
+
+<p>TOTAL = $$cartTotal</p>
+DISP;
+
+?>
 
 <form action="checkout.php" method="post">
 		<div>
--- a3/productarray.php
+++ a3/productarray.php
@@ -1,9 +1,9 @@
 <?php
 	//MAIN ARRAY
 	$products = array (
-        array("ID" => "1", "OID" => " ", "Title" => "Chain Loop Earrings", "Description" => "Earrings", "Option" => "One Size", "Price" =>"30", "Image" =>"ear.jpg"),
-        array("ID" => "2", "OID" => " ", "Title" => "Necklace", "Description" => "Necklace", "Option" => "One Size", "Price" =>"60", "Image" =>"necklace.jpg"),
-        array("ID" => "3", "OID" => "S", "Title" => "Buckle Belt", "Description" => "Belt", "Option" => "SMALL", "Price" =>"120", "Image" =>"belt.jpg"));
+        array("ID" => "1", "OID" => " ", "Title" => "Chain Loop Earrings", "Description" => "Earrings", "Option" => "One Size", "Price" =>"30", "Image" =>"media/ear.jpg"),
+        array("ID" => "2", "OID" => " ", "Title" => "Necklace", "Description" => "Necklace", "Option" => "One Size", "Price" =>"60", "Image" =>"media/necklace.jpg"),
+        array("ID" => "3", "OID" => "S", "Title" => "Buckle Belt", "Description" => "Belt", "Option" => "SMALL", "Price" =>"120", "Image" =>"media/belt.jpg"));
 
 	//PRODUCT ARRAYS
 	$product1 = $products[0];
